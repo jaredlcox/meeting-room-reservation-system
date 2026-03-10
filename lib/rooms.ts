a5a52ece7750b22fa77c521568c2ea73d369a43a
Jared Cox <jcox@ircuwd.com>
2026-03-10 09:52:56 -0400

    Update tsconfig.json to format library and path entries for better readability; include additional type definitions for development.

---
+++ lib/rooms.ts
@@ -0,0 +1,10 @@
+export const rooms = {
+    canvass: {
+      name: "Canvass Room",
+      email: "canvassroom@ircuwd.com",
+    },
+    sales: {
+      name: "Sales Room",
+      email: "salesroom@ircuwd.com",
+    },
+  }
